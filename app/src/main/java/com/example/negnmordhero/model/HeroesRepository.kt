6851f7c3fe1d5e641066267ec3630d22044c6b2e
mordovskoiisip231 <mordak2006@gmail.com>
2025-11-22 11:43:06 +0900

    Design for Mordovskoy

--- app/src/main/java/com/example/negnmordhero/model/HeroesRepository.kt
+++ app/src/main/java/com/example/negnmordhero/model/HeroesRepository.kt
@@ -5,9 +5,44 @@ import com.example.negnmordhero.R
 object HeroesRepository {
     val heroes = listOf(
         Hero(
-            nameRes = R.string.hero1,
+            nameRes = R.string.car1,
             descriptionRes = R.string.description1,
-            imageRes = R.drawable.hero1
+            imageRes = R.drawable.car1
+        ),
+        Hero(
+            nameRes = R.string.car2,
+            descriptionRes = R.string.description2,
+            imageRes = R.drawable.car2
+        ),
+        Hero(
+            nameRes = R.string.car3,
+            descriptionRes = R.string.description3,
+            imageRes = R.drawable.car3
+        ),
+        Hero(
+            nameRes = R.string.car4,
+            descriptionRes = R.string.description4,
+            imageRes = R.drawable.car4
+        ),
+        Hero(
+            nameRes = R.string.car5,
+            descriptionRes = R.string.description5,
+            imageRes = R.drawable.car5
+        ),
+        Hero(
+            nameRes = R.string.car6,
+            descriptionRes = R.string.description6,
+            imageRes = R.drawable.car6
+        ),
+        Hero(
+            nameRes = R.string.car7,
+            descriptionRes = R.string.description7,
+            imageRes = R.drawable.car7
+        ),
+        Hero(
+            nameRes = R.string.car8,
+            descriptionRes = R.string.description8,
+            imageRes = R.drawable.car8
         )
     )
 }
